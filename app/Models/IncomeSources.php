891d6359f236ebf108d7918ca9c190d749e443db
ThiagoFragaK <thiagofraga@proton.me>
2025-04-27 21:46:28 -0300

    Add: Models

---
+++ app/Models/IncomeSources.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IncomeSources extends Model
+{
+    /** @use HasFactory<\Database\Factories\IncomeSourcesFactory> */
+    use HasFactory;
+}
