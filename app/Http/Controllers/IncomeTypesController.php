a5e2a7fc30d9afe890871a0b6955737e04f6d08c
ThiagoFragaK <thiagofraga@proton.me>
2025-04-27 21:46:10 -0300

    Add: Controllers

---
+++ app/Http/Controllers/IncomeTypesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IncomeTypes;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IncomeTypesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IncomeTypes $incomeTypes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(IncomeTypes $incomeTypes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, IncomeTypes $incomeTypes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IncomeTypes $incomeTypes)
+    {
+        //
+    }
+}
