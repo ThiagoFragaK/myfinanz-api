691c0dce6aadae4899f652901728c83bfc9ef5dd
ThiagoFragaK <thiagofraga@proton.me>
2025-04-27 21:48:29 -0300

    Add: IncomeTypes seeders

--- database/seeders/IncomeTypesSeeder.php
+++ database/seeders/IncomeTypesSeeder.php
@@ -4,14 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class IncomeTypesSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('income_types')->insert([
+            ['name' => 'Regular'],
+            ['name' => 'Ticket Food'],
+            ['name' => 'Ticket Meals'],
+        ]);
     }
 }
