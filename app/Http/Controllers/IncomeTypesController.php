0fe7f450ecd80a18f8774302dd54dc9c3b4eb1b6
ThiagoFragaK <thiagofraga@proton.me>
2025-05-02 21:40:44 -0300

    Add: Logic in the incometypes controller

--- app/Http/Controllers/IncomeTypesController.php
+++ app/Http/Controllers/IncomeTypesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\IncomeTypes;
 use App\Http\Controllers\Controller;
 use App\Services\IncomeTypesService;
+use Ramsey\Uuid\Type\Integer;
 
 class IncomeTypesController extends Controller
 {
@@ -22,17 +23,40 @@ class IncomeTypesController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $status = $this->Service->createIncomeType([
+            "name" => $request->get("name"),
+            "created_at" => now(),
+        ]);
+
+        if($status)
+        {
+            return response()->json((object) [
+                "message" => "Income type created successfully",
+                "status_http" => 201
+            ]);
+        }
+
+        return response()->json((object) [
+            "message" => "Income type failed to be created.",
+            "status_http" => 500
+        ]);
     }
 
-    public function show(IncomeTypes $incomeTypes)
+    public function show(Int $id)
     {
-        //
+        return response()->json((object) [
+            "message" => "Income type has been returned successfully",
+            "data" => $this->Service->getIncomeTypeById($id),
+            "status_http" => 200
+        ]);
     }
 
-    public function edit(IncomeTypes $incomeTypes)
+    public function edit(Int $id, Request $request)
     {
-        //
+        return $this->Service->editIncomeType(
+            $id,
+            $request->get("name"),
+        );
     }
 
     public function update(Request $request, IncomeTypes $incomeTypes)
