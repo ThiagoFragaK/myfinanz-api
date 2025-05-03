c2980e68cb2acecbc9c7b00b5bc518ff13b04b64
ThiagoFragaK <thiagofraga@proton.me>
2025-05-02 21:41:10 -0300

    add: incomeservice logic

--- app/Services/IncomesService.php
+++ app/Services/IncomesService.php
@@ -2,30 +2,65 @@
 
 namespace App\Services;
 
+use App\Enums\StatusEnum;
+use App\Models\Incomes;
+
 class IncomesService
 {
-    public function createIncome()
+    public function getList()
     {
-
+        return Incomes::select("id", "name")->get();
     }
 
-    public function editIncome()
+    public function getIncomeById(Int $id)
     {
-
+        return Incomes::find($id);
     }
 
-    public function removeIncome()
+    public function createIncome(Array $incomeSource)
     {
-
+        Incomes::create($incomeSource);
+        return true;
     }
 
-    public function enableIncome()
+    public function editIncome(Int $id, String $name)
     {
-        
+        $income = $this->getIncomeById($id);
+        $income->update([
+            "name" => $name
+        ]);
+
+        return true;
     }
 
-    public function disableIncome()
+    public function removeIncome(Int $id)
     {
+        $income = $this->getIncomeById($id);
+        if($income->status === StatusEnum::Active->value)
+        {
+            return $this->disableIncome($id, $income);
+        }
+        else
+        {
+            return $this->enableIncome($id, $income);
+        }
+    }
 
+    private function enableIncome(Int $id, $income)
+    {
+        $income->update([
+            "status" => StatusEnum::Active->value,
+        ]);
+
+        return true;
+    }
+
+    private function disableIncome(Int $id, $income)
+    {
+        $income->update([
+            "status" => StatusEnum::Inactive->value,
+        ]);
+
+        return true;
     }
 }
