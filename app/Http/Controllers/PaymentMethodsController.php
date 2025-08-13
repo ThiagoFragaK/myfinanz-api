e6b9cc32b5e84bdf09316904dc682206f944ebdb
ThiagoFragaK <thiagofraga@proton.me>
2025-08-13 19:14:36 -0300

    Refactor: rename routes controller and services

---
+++ app/Http/Controllers/PaymentMethodsController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\PaymentMethodsService;
+
+class PaymentMethodsController extends Controller
+{
+    private $Service;
+    public function __construct()
+    {
+        $this->Service = new PaymentMethodsService();
+    }
+
+    public function get()
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'List retrieved successfully',
+            'data' => $this->Service->getList()
+        ], 200);
+    }
+
+    public function getPaymentMethodById(Int $id)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => "Payment method retrieved successfully",
+            'data' => $this->Service->getPaymentMethodById($id)
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $response = $this->Service->createPaymentMethod(
+            $request->get("name"),
+            $request->get("type"),
+            $request->get("turn_day"),
+            $request->get("limit"),
+        );
+        
+        if(isset($response['errors']))
+        {
+            return response()->json([
+                'success' => false,
+                'message' => "Failed to create Payment method",
+                'error' => $response['errors']
+            ], $response['http']);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => "Payment method created successfully",
+            'data' => $response
+        ], 201);
+    }
+
+    public function edit(Int $id, Request $request)
+    {
+        $payment = $this->Service->editPaymentMethod(
+            $id,
+            $request->get("name"),
+            $request->get("type"),
+            $request->get("turn_day"),
+            $request->get("limit"),
+        );
+        
+        if(isset($income['errors']))
+        {
+            return response()->json([
+                'success' => false,
+                'message' => "Failed to edit the Payment method",
+                'errors' => $income['errors']
+            ], $income['http']);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => "Payment method retrieved successfully",
+            'data' => $payment
+        ], 200);
+    }
+
+    public function disablePaymentMethod(Int $id)
+    {
+        $response =  $this->Service->disablePaymentMethod($id);
+        if(isset($response['errors']))
+        {
+            return response()->json([
+                'success' => false,
+                'message' => "Failed to disable Payment method",
+                'error' => $response['errors']
+            ], $response['http']);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => "Payment method disabled successfully",
+            'data' => $response
+        ], 201);
+    }
+
+    public function enablePaymentMethod(Int $id)
+    {
+        $response = $this->Service->enablePaymentMethod($id);
+        if(isset($response['errors']))
+        {
+            return response()->json([
+                'success' => false,
+                'message' => "Failed to enable Payment method",
+                'error' => $response['errors']
+            ], $response['http']);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => "Payment method enable successfully",
+            'data' => $response
+        ], 201);
+    }
+}
---
+++ app/Services/PaymentMethodsService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Services;
+use App\Models\PaymentMethods;
+use App\Enums\StatusEnum;
+class PaymentMethodsService
+{
+    public function getList()
+    {
+        return PaymentMethods::select('id', 'name', 'type', 'limit', 'turn_day', 'status')->get();
+    }
+
+    public function getPaymentMethodById(int $id)
+    {
+        return PaymentMethods::find($id);
+    }
+
+    public function createPaymentMethod(String $name, Int $type, Int $turnDay, Float $limit)
+    {
+        return PaymentMethods::create([
+            'name' => $name,
+            'turn_day' => $turnDay,
+            'limit' => $limit,
+            'type' => $type,
+            'status' => StatusEnum::Active->value,
+            'user_id' => 1,
+        ]);
+    }
+
+    public function editPaymentMethod(Int $id, String $name, Int $type, Int $turnDay, Float $limit)
+    {
+        $card = $this->getPaymentMethodById($id);
+        if(is_null($card))
+        {
+            return [
+                'errors' => "Failed to retrieve Card",
+                'http' => 404
+            ];
+        }
+
+        return $card->update([
+            'name' => $name,
+            'type' => $type,
+            'turn_day' => $turnDay,
+            'limit' => $limit,
+        ]);
+    }
+
+    public function disablePaymentMethod(Int $id)
+    {
+        $card = $this->getPaymentMethodById($id);
+        if(is_null($card))
+        {
+            return [
+                'errors' => "Failed to retrieve Card",
+                'http' => 404
+            ];
+        }
+
+        return $card->update([
+            'status' => StatusEnum::Inactive->value
+        ]);
+    }
+
+    public function enableCard(Int $id)
+    {
+        $card = $this->getPaymentMethodById($id);
+        if(is_null($card))
+        {
+            return [
+                'errors' => "Failed to retrieve Card",
+                'http' => 404
+            ];
+        }
+
+        return $card->update([
+            'status' => StatusEnum::Active->value
+        ]);
+    }
+}
