16479a36ab38886a715ce1f5c0b7b1abaf37855e
ThiagoFragaK <thiagofraga@proton.me>
2025-08-13 19:15:18 -0300

    refactor: migrations

--- database/migrations/2025_04_27_235141_create_payment_methods_table.php
+++ database/migrations/2025_04_27_235141_create_payment_methods_table.php
@@ -8,11 +8,12 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('cards', function (Blueprint $table) {
+        Schema::create('payment_methods', function (Blueprint $table) {
             $table->id();
             $table->string('name', 80);
             $table->integer('turn_day');
             $table->double('limit')->check('limit <= 999999');
+            $table->tinyInteger('type');
             $table->tinyInteger('status');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
