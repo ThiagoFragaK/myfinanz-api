985872712d10e69b5f5ab0f6142c5661fa70f4bf
ThiagoFragaK <thiagofraga@proton.me>
2025-08-18 19:37:13 -0300

    Add: Categories seeder

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categories;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $categories = [
+            ['name' => 'Food', 'description' => 'Expenses for food and restaurants', 'icon' => 'Hamburger'],
+            ['name' => 'Transport', 'description' => 'Expenses for transportation', 'icon' => 'Car'],
+            ['name' => 'Housing', 'description' => 'Rent, utilities, electricity, water', 'icon' => 'Home'],
+            ['name' => 'Health', 'description' => 'Health plans, medications, medical appointments', 'icon' => 'Activity'],
+            ['name' => 'Education', 'description' => 'School, courses and educational materials', 'icon' => 'Library'],
+            ['name' => 'Entertainment', 'description' => 'Movies, shows and leisure', 'icon' => 'Popcorn'],
+        ];
+
+        foreach ($categories as $category) {
+            Categories::create($category);
+        }
+    }
+}
